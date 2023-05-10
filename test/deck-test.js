bb302112f9a76659aeaea9fb4227f7bf80b9a795
Fernando Robles <fernandorobles97@yahoo.com>
2023-05-09 22:59:44 -0500

    refactor: Add beforeEach to consolidate variables

--- test/deck-test.js
+++ test/deck-test.js
@@ -2,27 +2,31 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const { createDeck, countCards } = require('../src/deck');
-const { prototypeData } = require('../src/data');
+const { createCard } = require('../src/card');
+const { sampleData } = require('../src/sample-data');
 
 describe('deck', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+
+  beforeEach(function() {
+    card1 = createCard(sampleData[0].id, sampleData[0].question, sampleData[0].answers, sampleData[0].correctAnswer);
+    card2 = createCard(sampleData[1].id, sampleData[1].question, sampleData[1].answers, sampleData[1].correctAnswer);
+    card3 = createCard(sampleData[2].id, sampleData[2].question, sampleData[2].answers, sampleData[2].correctAnswer);
+    deck = createDeck([card1, card2, card3]);
+  })
+
   it('should be a function', function() {
     expect(createDeck).to.be.a('function');
   });
 
   it('should create an arrray of card objects', function() {
-    const card1 = prototypeData[0];
-    const card2 = prototypeData[1];
-    const card3 = prototypeData[2];
-    const deck = createDeck([card1, card2, card3]);
-
     expect(deck).to.deep.equal([card1, card2, card3]);
   });
 
   it('should be able to count cards in deck', function() {
-    const card1 = prototypeData[0];
-    const card2 = prototypeData[1];
-    const card3 = prototypeData[2];
-    const deck = createDeck([card1, card2, card3]);
     const deck2 = createDeck([card1, card2]);
     const deck1Length = countCards(deck);
     const deck2Length = countCards(deck2);
