750cf4c37d271d6e37dc61c78dcfecca3d1d91df
Fernando Robles <fernandorobles97@yahoo.com>
2023-05-10 14:34:53 -0500

    feat: Add takeTurn function

--- src/round.js
+++ src/round.js
@@ -1,5 +1,18 @@
-const createRound = () => {};
+const createRound = (deck) => {
+  return {
+    deck: deck,
+    currentCard: deck[0],
+    turns: 0,
+    incorrectGuesses: []
+  }
+};
+
+const takeTurn = (guess, round) => {
+  round.turns += 1;
+  return round
+};
 
 module.exports = {
-  createRound
+  createRound,
+  takeTurn
 }
