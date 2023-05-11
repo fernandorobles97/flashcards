500d01f138982274314653d57f8e49a035a9977f
Fernando Robles <fernandorobles97@yahoo.com>
2023-05-11 17:17:15 -0500

    refactor: Use sample data for testing in card test

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const { sampleData } = require('../src/sample-data');
 const { createCard } = require('../src/card');
 
 describe('card', function() {
@@ -9,11 +10,11 @@ describe('card', function() {
   });
 
   it('should create a card and its properties', function() {
-    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card = createCard(sampleData[0].id, sampleData[0].question, sampleData[0].answers, sampleData[0].correctAnswer);
     
-    expect(card.id).to.equal(1);
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-    expect(card.correctAnswer).to.equal('object');
+    expect(card.id).to.equal(sampleData[0].id);
+    expect(card.question).to.equal(sampleData[0].question);
+    expect(card.answers).to.deep.equal(sampleData[0].answers);
+    expect(card.correctAnswer).to.equal(sampleData[0].correctAnswer);
   });  
 });
